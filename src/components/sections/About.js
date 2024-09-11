a320c032491701ba9cd7b2be34193c8a3cf254b7
alan <alanl07905@gmail.com>
2024-09-11 15:08:02 -0400

    attempted to fix browserouter not working

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -18,7 +18,7 @@ import {
 
 const About = () => {
   return (
-	  <div>
+    <div>
       <p className={Spacing}>
         I am currently interested in software engineering, data science, and machine learning. 
         During my free time, I spend time learning new technologies and frequently working on 
@@ -26,13 +26,10 @@ const About = () => {
       </p>
 
       <ol>
-
         <li className={Spacing}>
           <div className={Container}>
             <div className={DivStyles} />
-            <header className={Subhead}>
-              Sept 2022 - May 2026
-            </header>
+            <header className={Subhead}>Sept 2022 - May 2026</header>
             <div className={Info}>
               <h3 className={SectionH3}>
                 <div>
@@ -55,16 +52,9 @@ const About = () => {
               </h3>
               <p className={InfoP}>
                 <b>Courses: </b> 
-                  Software Engineering,
-                  Web Application Development,
-                  Analysis of Algorithms,
-                  Functional Programming,
-                  Statistics,
-                  Computer Systems,
-                  Linear Algebra,
-                  Discrete Math,
-                  Data Structures & OOP in Java
-                <br />
+                  Software Engineering, Web Application Development, Analysis of Algorithms,
+                  Functional Programming, Statistics, Computer Systems, Linear Algebra,
+                  Discrete Math, Data Structures & OOP in Java
               </p>
             </div>
           </div>
@@ -77,19 +67,17 @@ const About = () => {
             <div className={Info}>
               <h3 className={SectionH3}>
                 <div>
-                  <a className={H3Container} href='/courses' rel='noreferrer' onClick={() => window.scroll(0,0)}>
+                  <Link className={H3Container} to='/courses' onClick={() => window.scroll(0, 0)}>
                     <span className={H3Span} />
-                    <Link className={LinkWrapper} to='/courses' onClick={() => window.scroll(0,0)}>
-                      <span>
-                        <span className='whitespace-nowrap'>
-                          <span className={LinkText}> View Relevant Courses</span>
-                          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' className={RightArrowSvg}>
-                            <path fillRule='evenodd' d='M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z' clipRule='evenodd' />
-                          </svg>
-                        </span>
+                    <Link className={LinkWrapper} to='/courses' onClick={() => window.scroll(0, 0)}>
+                      <span className='whitespace-nowrap'>
+                        <span className={LinkText}>View Relevant Courses</span>
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' className={RightArrowSvg}>
+                          <path fillRule='evenodd' d='M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z' clipRule='evenodd' />
+                        </svg>
                       </span>
                     </Link>
-                  </a>
+                  </Link>
                 </div>
               </h3>
             </div>
@@ -99,9 +87,7 @@ const About = () => {
         <li className={Spacing}>
           <div className={Container}>
             <div className={DivStyles} />
-            <header className={Subhead}>
-              Sept 2018 - June 2022
-            </header>
+            <header className={Subhead}>Sept 2018 - June 2022</header>
             <div className={Info}>
               <h3 className={SectionH3}>
                 <div>
@@ -115,7 +101,7 @@ const About = () => {
                       />
                       Forest Hills High School
                       <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' className={LinkSvg}>
-                        <path fillRule='evenodd' d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z' clipRule='evenodd'/>
+                        <path fillRule='evenodd' d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z' clipRule='evenodd' />
                       </svg>
                     </span>
                   </a>
@@ -126,29 +112,20 @@ const About = () => {
                 <b>GPA:</b> 102.95 | <b>Rank:</b> 7/960 <br />
                 <b>Activities: </b> Cross Country Team, Indoor Track & Field Team <br />
                 <b>Diploma: </b> Regents Advanced Designation with Honor, Regents Designation with Mastery in Science, Regents Advanced Designation with Mastery in Math <br />
-                <b>Awards:</b> 
+                <b>Awards:</b>
               </p>
               <ul className='list-disc ml-4 text-sm'>
-                <li>
-                  President's Education Award for Excellence Gold Seal
-                </li>
-                <li>
-                  Outstanding Academic Excellence (GPA 100+)
-                </li>
-                <li>
-                  United States Marine Scholastic Excellence Award
-                </li>
-                <li>
-                  Forest Hills High School Scholar Athlete
-                </li>
+                <li>President's Education Award for Excellence Gold Seal</li>
+                <li>Outstanding Academic Excellence (GPA 100+)</li>
+                <li>United States Marine Scholastic Excellence Award</li>
+                <li>Forest Hills High School Scholar Athlete</li>
               </ul>
             </div>
           </div>
         </li>
-
       </ol>
     </div>
-  )
-}
+  );
+};
 
 export default About;
