29e0afce9e04dcdb912efd31d02dafbc7dd7ad48
alan <alanl07905@gmail.com>
2024-10-04 21:58:24 -0400

    minor change

--- src/components/courses/Junior.js
+++ src/components/courses/Junior.js
@@ -84,7 +84,7 @@ const Junior = () => {
 								<a className={H3Container} href='https://www.bu.edu/academics/cas/courses/cas-cs-350/' target='_blank' rel='noreferrer'>
 									<span className={H3Span} />
 									<span>
-										CS 350 - Distributed Systems
+										CS 350 - Computing Systems
 										<span className='inline-block'>
 											<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' className={LinkSvg}>
 												<path fillRule='evenodd' d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z' clipRule='evenodd' />
