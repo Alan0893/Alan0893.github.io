246d46c8a6106378773bd6bce475d87533c4866e
alan <alanl07905@gmail.com>
2024-11-06 15:21:18 -0500

    added assignment 7

--- src/pages/Assignments.js
+++ src/pages/Assignments.js
@@ -19,6 +19,11 @@ import {
 
 const assignments = [
   {
+    title: 'Hypothesis Testing and Confidence Intervals in Linear Regression',
+    description: 'Enhanced interactive webpage to allow users to perform hypothesis tests on the slope or intercept of the regression line and generate confidence intervals based on simulations.',
+    repoLink: 'https://github.com/Alan0893/alanl193-assignment-7',
+    videoLink: '/assets/videos/assignment7.mp4'
+  }, {
     title: 'Linear Regression',
     description: 'Created an interactive webpage to demonstrate how modifying parameters affects regression results, especially when there is no actual relationship between X and Y.',
     repoLink: 'https://github.com/Alan0893/alanl193-assignment-6',
